d9706e698f80399673bc139aab6c935ab3c22545
Abarham97 <abodzbarham@gmailcom>
2023-09-13 19:20:33 +0300

    init

---
+++ src/test/java/com/Abarham97/jBCrypt/JBCryptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Abarham97.jBCrypt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JBCryptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
